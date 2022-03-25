11193211ea65ef31365b880f50119c4acda29d15
Flavio Macedo <fhenrique609@gmail.com>
2022-03-25 12:43:57 -0300

    Criando função de cadastro e login

---
+++ autenticacao/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def cadastro(request):
+    return HttpResponse('Cadastro')
+
+
+def login(request):
+    return HttpResponse('Login')
