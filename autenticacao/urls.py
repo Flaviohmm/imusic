2c74eae216688797baabec5037e7155c3c9e25a4
Flavio Macedo <fhenrique609@gmail.com>
2022-03-26 10:12:33 -0300

    Password Reset Templates

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -7,8 +7,24 @@ urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"),
     path('login/', views.login, name="login"),
     path('sair/', views.sair, name="sair"),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path(
+        'reset_password/', 
+        auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), 
+        name="reset_password"
+    ),
+    path(
+        'reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"
+    ),
+    path(
+        'reset/<uidb64>/<token>', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'), 
+        name="password_reset_confirm"
+    ),
+    path(
+        'reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"
+    ),
 ]
