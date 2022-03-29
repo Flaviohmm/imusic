79f6f306a20cec2f0fe2d81e58567ba66adfa917
Flavio Macedo <fhenrique609@gmail.com>
2022-03-29 10:28:08 -0300

    Criando a página home

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect
+)
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django import forms
+
+from .models import Album, Musica
 
 # Create your views here.
 
 
 @login_required(login_url='/auth/login')
 def home(request):
-    return HttpResponse('Home')
+    # Mostrar todos os álbuns em ordem cronológica de upload
+    albuns = Album.objects.all()
+    return render(request, 'home.html', {'albuns': albuns})
